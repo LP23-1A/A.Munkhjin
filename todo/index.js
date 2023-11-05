fc11a133ba1ad0a9d58fc29f536354cf0756ef84
Munkhjin11x <147012832+Munkhjin11x@users.noreply.github.com>
2023-11-05 12:26:47 +0800

    openmodal

--- todo/index.js
+++ todo/index.js
@@ -1,122 +1,106 @@
-let progress1 = ["To do", "In progress", "Stuck", "Done"];
-
-let body = document.querySelector("body");
-let container = document.createElement("div");
-container.classList.add("container");
-body.appendChild(container);
-let modalcard = document.createElement("div");
-  let header = document.createElement("div");
-  let title = document.createElement("div");
-  let titleInput = document.createElement("input");
-  let description = document.createElement("div");
-  let desInput = document.createElement("input");
-  let status = document.createElement("div");
-  let statusbtn = document.createElement("button");
-  let priority = document.createElement("div");
-  let prioritybtn = document.createElement("button");
-
-  modalcard.classList.add("modalcard");
-  header.classList.add('header');
-  title.classList.add('title')
-  titleInput.classList.add('titleinput')
-  description.classList.add('description')
-  desInput.classList.add('desInput')
-  status.classList.add('status')
-  statusbtn.classList.add('statusbtn')
-  priority.classList.add('priorrity')
-  prioritybtn.classList.add('prioritybtn')
-
-
-  header.innerText = "Addcard";
-  title.innerText = "Title";
-  description.innerText = "Description";
-  status.innerText = "Status";
-  priority.innerText = "Priority";
-
-  container.appendChild(modalcard);
-  modalcard.appendChild(header);
-  modalcard.appendChild(title);
-  modalcard.appendChild(titleInput);
-  modalcard.appendChild(description);
-  modalcard.appendChild(desInput);
-  modalcard.appendChild(status);
-  modalcard.appendChild(statusbtn);
-  modalcard.appendChild(priority);
-  modalcard.appendChild(prioritybtn);
-
-
-function modal() {
-  
-  prioritybtn.innerText = "Add task";
-
-  let taskInput = [];
-
-  let obj = { 
-    title: "To do",
-    description: "this is todo card",
-    priority: "high"
-
-
-};
-
-  function titleChangeEvent(event) {
-    obj.title = event.target.value;
-
-    console.log(event.target.value);
+let progress = [
+    {
+      title: "Todo",
+    },
+    {
+      title: "in progress",
+    },
+    {
+      title: "stuck",
+    },
+    {
+      title: "done",
+    },
+  ];
+  function createcards(progress) {
+    let { title } = progress;
+    return `<div>
+    <h1>${title}</h1>
+     <div class="card"></div>
+    <button class='addcard'>+addcard</button> 
+    </div>`;
   }
-
-  // titleInput.onchange = titleChangeEvent;
-
-  titleInput.addEventListener("change", (event) => {
-    titleChangeEvent(event);
+  
+  function listItem(props) {
+    let { title, desc } = props;
+    return `<div>
+    <h3>${title}</h3>
+    <p>${desc}</p>
+    </div
+    `;
+  }
+  
+  let body = document.querySelector("body");
+  let container = document.querySelector(".container");
+  let cards = document.querySelector(".cards");
+  let openModal = document.querySelector('.openmodal');
+  let addCardOpen = document.querySelector(".addcard");
+  
+  console.log(addCardOpen);
+  function openmodal() {
+    return `
+    <div class="openmodal">
+    <h2>Add task</h2>
+    <h3>Title</h3>
+    <input type="">
+    <h3>description</h3>
+    <input type="">
+    <h3>status</h3>
+    <select id="status">
+  <option value="todo">todo</option>
+  <option value="inprogress">inprogress</option>
+  <option value="stuck">stuck</option>
+  <option value="done">done</option>
+     </select>
+     <p>priority</p>
+     <select id="priority">
+     <option value="high">high</option>
+     <option value="low">low</option>
+        </select>
+        <button></button>
+    </div>`
+   
+  }  
+  let data = [
+    {
+      title: "Todo",
+      desc: "123",
+      status: "done"
+    },
+    {
+      title: "Todo",
+      desc: "123",
+      status: "inprogress"
+    },
+    {
+      title: "Todo",
+      desc: "123",
+      status: "stuck"
+    },
+    {
+      title: "todo",
+      desc: "123",
+      status: "todo"
+  
+    },
+  ];
+  progress.map((a) => {
+    cards.innerHTML += createcards(a);
   });
 
-  function submitHandler() {
-    taskInput.push(obj);
-  }
-
-  prioritybtn.onclick = submitHandler;
-
-  console.log(taskInput);
-}
-
-function createcards(progress) {
-  let card = document.createElement("div");
-  let addbtn = document.createElement("button");
-  let plusdiv = document.createElement("div");
-  let heads = document.createElement("p");
-  let cardtitle = document.createElement("p");
-  let carddesc = document.createElement("p");
-
-  //modal
-  card.classList.add("card");
-  addbtn.classList.add("addbtn");
-  plusdiv.classList.add("plusdiv");
-  heads.classList.add('heads');
-  cardtitle.classList.add('cardtitle');
-  carddesc.classList.add('carddesc');
-
-  card.appendChild(heads);
-  card.appendChild(plusdiv);
-  plusdiv.appendChild(cardtitle);
-  plusdiv.appendChild(carddesc);
-  card.appendChild(addbtn);
-  // console.log(card);
-  container.appendChild(card);
-  heads.innerText = progress;
-  addbtn.innerText = '+ Add card'
-
-  addbtn.onclick = () => {
-    let modalopen = document.getElementsByClassName("modalcard");
-    modalopen[0].style.display = "flex";
-  };
-  plusdiv.innerText = 'todo';
-}
-
-function drawcards(array) {
-  for (let i = 0; i < array.length; i++) {
-    createcards(array[i]);
-  }
-}
-modal();
-drawcards(progress1);
+  
+  data.map((e) => {
+    let card = document.querySelectorAll('.card');
+    if(e.status === "todo") {
+      card[0].innerHTML += listItem(e);
+    } else if(e.status === "inprogress"){
+      card[1].innerHTML += listItem(e);
+    }
+    else if(e.status === "stuck"){
+      card[2].innerHTML += listItem(e);
+    }
+    else if(e.status === "done"){
+      card[3].innerHTML += listItem(e);
+    }
+  });
+  openModal.innerHTML += openmodal()
