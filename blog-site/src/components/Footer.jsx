dd4573cdebfd7af4d6667338853a8b1443672fdd
Munkhjin11x <147012832+Munkhjin11x@users.noreply.github.com>
2023-12-04 23:20:58 +0800

    footer hiigduulcihsen

---
+++ blog-site/src/components/Footer.jsx
@@ -0,0 +1,49 @@
+import { Facebook, Twitter, Instagram, Linkedin } from "@/Icons/SocialIcons";
+import MetaBLogIcon from "@/Icons/MetablogIcon";
+export default function Footer() {
+  return (
+    <footer className="bg-[#F6F6F7] pt-[64px] px-[352px]">
+      <div className=" flex justify-between">
+        <div>
+          <p>About</p>
+          <p className="mt-[12px] w-[280px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut  <br />enim
+            ad minim veniam
+          </p>
+          <p className="mt-[24px]">
+            Email: <span> info@jstemplate.net</span>
+          </p>
+          <p>
+            Phone: <span>880 123 456 789</span>
+          </p>
+        </div>
+        <div className="flex flex-col gap-[8px]">
+          <p>Home</p>
+          <p>Blog</p>
+          <p>Contact</p>
+        </div>
+        <div className="flex gap-[26px]">
+            <Facebook/>
+            <Twitter/>
+            <Instagram/>
+            <Linkedin/>
+        </div>
+      </div>
+      <hr />
+      <div className=" flex justify-between items-center py-[32px]">
+        <div>
+        <MetaBLogIcon/>
+        <p>
+        © All Rights Reserved.
+        </p>
+        </div>
+        <div className="flex gap-[10px]">
+            <p>Terms of Use</p>
+            <p>Privacy Policy</p>
+            <p>Cookie Policy</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- blog-site/src/pages/index.js
+++ blog-site/src/pages/index.js
@@ -1,6 +1,7 @@
-import  { Card,ArticleProps } from "@/components/Article";
+import { Card, ArticleProps } from "@/components/Article";
 import Trend from "@/components/Trending";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 const dataCard = [
   {
     img: "https://s3-alpha-sig.figma.com/img/3abf/26dd/585632b9d05dcfd0daffacedd55842f5?Expires=1702857600&Signature=GtIcIcyrku03di6uhi0mfVSqHwIdIsUNoUQebv4ln3q84G9ZINAh~g63In7v1hCCoAUoAP2aTUyCsm0UHjL-VzZqeJwqljXEBvlG38XfWgPV0jOZS41GSMMySeIgQ-5a6beEdkq3-nVqA8DtYQ5tFNSBjsKIZZlJohL6yXlNmptCXdFuApfCK6nWzmbfiNgeW-g3FK0QgNJNPlJaJFhYBW7ic31MDvl416i18XVjJb3BD8ahSb1F7OqOffjqQOGVpbAL4r4vPSLE8tV4YbaMpt1sknJsMh0hxR4lDqQ0txf88uFFFPq7xf9FpZyaEIntEtG8EVMtjjmahArIWExyCw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
@@ -10,11 +11,13 @@ const dataCard = [
     date: "August 20, 2022 ",
   },
 ];
-const dataTrending =[{
-  img:'https://s3-alpha-sig.figma.com/img/e8eb/3bce/c766a697a30822ccba768b03c5125ead?Expires=1702857600&Signature=mS1K5kWMKdlerCnD04RJL-32u2QR~P-BrzhJjc60wD~2aECa~ybTYlb3shy-lxMN5NrwHvXXBV5G5d9aoWO1RLwzHwAtF0728X2AY0vR0eOwB5zNxaMBkSM565DVEfM7aE0JTI3wA-wOYHs2JrtG5vy1d4qxM-adv6sPFGBeQVH7FTrfU-wOaagqWmRzlotOv1cxHZET7oPgA80Qf-g5BaxlL9l4Z74welNJLRWmUTygXksPWe7bdolzVHy5EBdWJ6r3hfazxLpk-mtePX1ea2lBTLfw1o~vw~3SZmhoJzDxCSz5OJdJkb8H1y3AMv9Q1N6QBuWNe2rCVdR6QeHDmQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
-  tag:"Techonlogy"
-  ,desc:"The Impact of Technology on the Workplace: How Technology is Changing"
-}]
+const dataTrending = [
+  {
+    img: "https://s3-alpha-sig.figma.com/img/e8eb/3bce/c766a697a30822ccba768b03c5125ead?Expires=1702857600&Signature=mS1K5kWMKdlerCnD04RJL-32u2QR~P-BrzhJjc60wD~2aECa~ybTYlb3shy-lxMN5NrwHvXXBV5G5d9aoWO1RLwzHwAtF0728X2AY0vR0eOwB5zNxaMBkSM565DVEfM7aE0JTI3wA-wOYHs2JrtG5vy1d4qxM-adv6sPFGBeQVH7FTrfU-wOaagqWmRzlotOv1cxHZET7oPgA80Qf-g5BaxlL9l4Z74welNJLRWmUTygXksPWe7bdolzVHy5EBdWJ6r3hfazxLpk-mtePX1ea2lBTLfw1o~vw~3SZmhoJzDxCSz5OJdJkb8H1y3AMv9Q1N6QBuWNe2rCVdR6QeHDmQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    tag: "Techonlogy",
+    desc: "The Impact of Technology on the Workplace: How Technology is Changing",
+  },
+];
 
 const dataTag = [
   {
@@ -35,24 +38,23 @@ const dataTag = [
 ];
 console.log(dataTrending);
 export default function Home() {
- 
   return (
-    <main className="w-full r">
-       <Navbar/>
-       <div className=" flex justify-center">
-        {dataTrending.map((e)=>{
-          return ( <div>
-            <Trend img={e.img} desc={e.desc} tag={e.tag}/>
-          </div>
-            
-          )
+    <main className="w-full ">
+      <Navbar />
+      <div className=" flex justify-center">
+        {dataTrending.map((e) => {
+          return (
+            <div>
+              <Trend img={e.img} desc={e.desc} tag={e.tag} />
+            </div>
+          );
         })}
-       </div>
-      <div className=" flex justify-between ">
-        <div className="flex gap-10"> 
+      </div>
+      <div className=" flex justify-between px-[352px]">
+        <div className="flex gap-10">
           <h2>All</h2>
           {dataTag.map((e) => {
-            return < ArticleProps tag={e.tag} />;
+            return <ArticleProps tag={e.tag} />;
           })}
         </div>
         <h2>View All</h2>
@@ -62,6 +64,7 @@ export default function Home() {
           return <Card img={e.img} tag={e.tag} title={e.title} date={e.date} />;
         })}
       </div>
+      <Footer />
     </main>
   );
 }
